d19f04195098c7fdbf4fac6acea2e08db099d4d6
Danika Pearson <89046934+karmadog72@users.noreply.github.com>
2021-09-27 12:23:10 -0600

    added functions

--- script.js
+++ script.js
@@ -14,3 +14,13 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 //test push
+function generatePassword() {
+  var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+  var number = "1234567890";
+  var symbols = "!@#$%^&*()+";
+  var passwordLength = parseInt(prompt("Enter password length?"));
+  if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
+    return "Invalid password length. Please enter valid password between 8-128 characters";
+  }
+}

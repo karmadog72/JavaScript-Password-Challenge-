01d07e567a584e224b52e3a2075994de7f014a0e
Danika Pearson <89046934+karmadog72@users.noreply.github.com>
2021-09-27 12:08:29 -0600

    test

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // Assignment code here
 
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -10,8 +9,8 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+//test push
